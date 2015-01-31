0e927855d4352d5acdf17d2a06c9e7397aed7bc4
Larry Eliemenye <denachural@gmail.com>
2015-01-31 11:59:41 +0000

    Refactored metadata generator

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var EpubStream = require("./lib/core");
-var eStream = new EpubStream({title:"things fall apart"});
+var eStream = new EpubStream({
+    title:"Far from a maddening crowd",
+    author:"Larry Eliemenye"
+});
 eStream.create("../mybooks");
 
 module.exports = eStream;
