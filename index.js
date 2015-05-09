fd3c69daae8e8dba8885c09413678faa44a4a876
Larry Eliemenye <denachural@gmail.com>
2015-05-09 01:44:40 +0100

    Some Changes to how epubs are generated

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var EpubStream = require("./lib/core");
 var eStream = new EpubStream({
-    title:"Far from a maddening crowd",
-    author:"Dagmara Eliemenye"
+    title:"Eze goes to school",
+    author:"Chuma Ekebe"
 });
-eStream.create(function(err, epub){
-    console.log(err, epub)
+eStream.create(function(err, ctx){
+    eStream.pack(ctx, function(err, epub){
+        if(err) throw err;
+        console.log(err, epub);
+    });
 });
+
+
 
 module.exports = eStream;
